cbeb3e5c0d3c9167d7a9883d9d932e510e999e55
unknown <dennisw820@gmail.com>
2022-07-27 12:25:54 -0400

    Added CORSsupport.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 // Dependencies 
 const express = require('express');
-const router = require('./api/routes/router.js.js.js');// Contact
+const router = require('./api/routes/router.js.js.js');
 // const mongodb = require('./.config/mongodb');
 const passport = require('./.config/passport.js');
 const mysql = require('mysql');
 var cookieSession = require('cookie-session');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
     // Middleware
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
+    app.use(cors());
     // require static for static routes
     app.use(express.static(__dirname));
     app.use(express.static('private'));
